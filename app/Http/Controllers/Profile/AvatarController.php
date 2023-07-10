67ec88bd6d2623f06d985d725a5de1f3cc9d51e7
SALMAN AJANI <salmanajani0426@gmail.com>
2023-07-10 13:18:17 +0530

    added user avatar adding feature

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request)
+    {
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+
+        if ($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
+        Auth::user()->update(['avatar' => $path]);
+
+        return Redirect::route('profile.edit')->with('message', 'Avatar is updated');
+    }
+}
---
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -0,0 +1,31 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('User Avatar') }}
+        </h2>
+        <img width="100" height="100" class="rounded" src="{{ "/storage/$user->avatar" }}" alt="user-avatar">
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Add or update user avatar.') }}
+        </p>
+    </header>
+
+    @if (session('message'))
+        <div class="text-red-500">
+            {{ session('message') }}
+        </div>
+    @endif
+    <form method="post" action="{{ route('profile.avatar') }}" enctype="multipart/form-data">
+        @csrf
+        @method('patch')
+        <div>
+            <x-input-label for="avatar" :value="__('Avatar')" />
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" required autofocus
+                autocomplete="avatar" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div>
+
+        <div class="flex items-center gap-4 mt-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+        </div>
+    </form>
+</section>
